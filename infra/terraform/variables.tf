74849c12f55010e68f5d7de5ed5c0e5ad6ec00f1
Sukhraj Singh <mahalraj0001@gmail.com>
2026-09-12 20:30:58 -0400

    Create variables.tf

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" { type = string; default = "ca-central-1" }
+variable "project" { type = string; default = "deployguard" }
+variable "environment" { type = string; default = "production" }
+variable "kubernetes_version" { type = string; default = "1.30" }
