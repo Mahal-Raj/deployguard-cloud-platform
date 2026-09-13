47e3fa2378b8efd9c176a194b0dacd935f8a993e
Sukhraj Singh <mahalraj0001@gmail.com>
2026-09-12 20:30:54 -0400

    Create outputs.tf

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "cluster_name" { value = aws_eks_cluster.main.name }
+output "cluster_endpoint" { value = aws_eks_cluster.main.endpoint; sensitive = true }
+output "ecr_repository_url" { value = aws_ecr_repository.app.repository_url }
